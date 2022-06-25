af724e061bd9d0871d8e4e7814794f2c547ffb4a
박성재 <di2ework@naver.com>
2022-06-26 01:38:08 +0900

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { Suspense } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
-import NavBar from './components/NavBar';
-import NotFound from './pages/NotFound';
-import MainPage from './pages/main/MainPage';
-import LoginPage from './pages/login/LoginPage';
-import TodolistPage from './pages/todolist/TodolistPage';
-import PostPage from './pages/post/PostPage';
-
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import theme from "./theme";
+import NotFound from "./pages/NotFound";
+import MainPage from "./pages/main/MainPage";
+import LoginPage from "./pages/login/LoginPage";
+import TodolistPage from "./pages/todolist/TodolistPage";
+import PostPage from "./pages/post/PostPage";
+import NavBar from "./components/NavBar";
 
 function App() {
-    return (
-        <React.Fragment>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Suspense fallback={(<div>Loading...</div>)}>
-                    <Router>
-                        <Routes>
-                            <Route path="/" element={<MainPage />} />
-                            <Route path="/login" element={<LoginPage />} />
-                            <Route path="/todolist" element={<TodolistPage />} />
-                            <Route path="/post" element={<PostPage />} />
-                            <Route path="/*" element={<NotFound />} />
-                        </Routes>
-                    </Router>
-                </Suspense>
-            </ThemeProvider>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/todolist" element={<TodolistPage />} />
+              <Route path="/post" element={<PostPage />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+            <NavBar />
+          </Router>
+        </Suspense>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default App;
