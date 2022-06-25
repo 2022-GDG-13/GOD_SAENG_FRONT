be41bfc531405d2beba1a6691b5fd2bd901d557c
0hee0 <suhee0206@gmail.com>
2022-06-26 00:55:54 +0900

    Add Calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Typography, Grid } from '@mui/material';
+import Godsaeng from '../assets/images/Godsaeng.png';
 
 
-function Calendar() {
+function Calendar(props) {
+    const dayOfWeeks = ["일", "월", "화", "수", "목", "금", "토"];
+    const [days, setDays] = useState([
+        {"day": 1, "godsaeng": true},
+        {"day": 2, "godsaeng": false},
+        {"day": 3, "godsaeng": false},
+        {"day": 4, "godsaeng": false},
+        {"day": 5, "godsaeng": false},
+        {"day": 6, "godsaeng": false},
+        {"day": 7, "godsaeng": false},
+        {"day": 8, "godsaeng": false},
+        {"day": 9, "godsaeng": false},
+        {"day": 10, "godsaeng": false},
+        {"day": 11, "godsaeng": false},
+        {"day": 12, "godsaeng": false},
+        {"day": 13, "godsaeng": false},
+        {"day": 14, "godsaeng": false},
+        {"day": 15, "godsaeng": false},
+        {"day": 16, "godsaeng": false},
+        {"day": 17, "godsaeng": false},
+        {"day": 18, "godsaeng": false},
+        {"day": 19, "godsaeng": false},
+        {"day": 20, "godsaeng": false},
+        {"day": 21, "godsaeng": false},
+        {"day": 22, "godsaeng": false},
+        {"day": 23, "godsaeng": false},
+        {"day": 24, "godsaeng": false},
+        {"day": 25, "godsaeng": false},
+        {"day": 26, "godsaeng": false},
+        {"day": 27, "godsaeng": false},
+        {"day": 28, "godsaeng": false},
+        {"day": 29, "godsaeng": false},
+        {"day": 30, "godsaeng": false},
+    ]);
+
     return (
         <div>
-            
+            <Grid container>
+                {dayOfWeeks.map(dayOfWeek => 
+                    <Grid key={dayOfWeek} item xs={12/7} pb="0.25rem">
+                        <Typography fontSize="14px" textAlign="center" color="#94A3B8">{dayOfWeek}</Typography>
+                    </Grid>
+                )}
+                {Array(3).fill(1).map(day =>
+                    <Grid key={day} item xs={12/7} display="flex" flexDirection="column" alignItems="center" sx={{ height: "56px" }} />
+                )}
+                {days?.map(day =>
+                    <Grid 
+                        key={day} item xs={12/7} 
+                        display="flex" flexDirection="column" alignItems="center" sx={{ height: "56px", backgroundColor: props.day===day.day ? "primary.light" : "none" }}
+                        onClick={props.handleDay(day?.day)}
+                    >
+                        <Typography fontSize="14px">{day.day}일</Typography>
+                        {day.godsaeng &&
+                            <img src={Godsaeng} width="24px" />
+                        }
+                    </Grid>
+                )}
+            </Grid>
         </div>
     )
 }
--- src/pages/post/PostPage.js
+++ src/pages/post/PostPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TextField, Typography, Button, Stack, Box } from '@mui/material';
 import { makeStyles, styled } from '@mui/styles';
 import { CloudUploadIcon } from '@heroicons/react/outline';
@@ -7,8 +7,36 @@ import Calendar from '../../components/Calendar';
 
 function PostPage() {
     const classes = useStyles();
+
     const [stage, setStage] = useState(1);
+    const [disabled, setDisabled] = useState(true);
+
+    const [day, setDay] = useState(undefined);
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
     const [file, setFile] = useState();
+    
+    const [tasks, setTasks] = useState([]);
+
+
+    useEffect(() => {
+        if (stage == 2) {
+            // TODO: POST day and get tasks
+            setTasks([
+
+            ]);
+        }
+        else if (stage == 3) {
+            // TODO: POST post image, title, content
+        }
+    }, [stage]);
+
+    useEffect(() => {
+        if (title) {
+            setDisabled(false);
+        }
+    }, [title]);
 
     const handleFileChange = (index) => (event) => {
         event.preventDefault();
@@ -18,6 +46,14 @@ function PostPage() {
 
     const handleStage = (event) => {
         setStage(stage+1);
+        setDisabled(true);
+    }
+
+    const handleDay = (day) => (event) => {
+        if (day) {
+            setDay(day);
+            setDisabled(false);
+        }
     }
 
     const handleUpload = (event) => {
@@ -34,36 +70,42 @@ function PostPage() {
                 </Typography>
             </Stack>
 
-            {stage == 3 &&
-                <Calendar />
-            }
+            <Box>
+                {stage == 1 &&
+                    <Stack spacing={"1.5rem"}>
+                        <Typography color="#64748B" mb="1.5rem">게시글을 작성할 날짜를 선택해주세요.</Typography>
+                        <Typography color="primary.dark" fontWeight="700" variant="h5" pb="0.5rem">6월</Typography>
+                        <Calendar handleDay={handleDay} day={day} />
+                    </Stack>
+                }
 
-            {stage == 2 &&
-                <Stack spacing={"1.5rem"}>
-                    <TextField fullWidth variant="standard" placeholder="제목" />
-                    <Box backgroundColor="#F8FAFC" sx={{ border: "1px solid #E2E8F0", borderRadius: "10px", p: "4rem", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                        <input type="file" id="contained-button-file" accept="image/*" onChange={handleFileChange} style={{ display: "none" }} />
-                        <label htmlFor="contained-button-file">
-                            <Stack alignItems="center">
-                                <CloudUploadIcon style={{ width: "30px" }} />
-                                <Typography>이미지 업로드</Typography>
-                            </Stack>
-                        </label>   
-                    </Box>
-                    <TextField fullWidth multiline minRows={16} placeholder="글을 작성해주세요" />
-                </Stack>
-            }
+                {stage == 2 &&
+                    <Stack spacing={"1.5rem"}>
+                        <TextField fullWidth variant="standard" placeholder="제목" onChange={(e) => setTitle(e.target.value)} />
+                        <Box backgroundColor="#F8FAFC" sx={{ border: "1px solid #E2E8F0", borderRadius: "10px", p: "4rem", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                            <input type="file" id="contained-button-file" accept="image/*" onChange={handleFileChange} style={{ display: "none" }} />
+                            <label htmlFor="contained-button-file">
+                                <Stack alignItems="center">
+                                    <CloudUploadIcon style={{ width: "30px" }} />
+                                    <Typography>이미지 업로드</Typography>
+                                </Stack>
+                            </label>   
+                        </Box>
+                        <TextField fullWidth multiline minRows={16} placeholder="글을 작성해주세요" onChange={(e) => setContent(e.target.value)} />
+                    </Stack>
+                }
 
-            {stage == 3 &&
-                <div>Tasks</div>
-            }
+                {stage == 3 &&
+                    <div>Tasks</div>
+                }
+            </Box>
 
             {stage == 3 ? (
                 <Button variant="contained" fullWidth mt="1.5rem" onClick={handleUpload}>
                     <Typography color="#fff">업로드</Typography>
                 </Button>
             ) : (
-                <Button variant="contained" fullWidth mt="1.5rem" onClick={handleStage}>
+                <Button variant="contained" fullWidth mt="1.5rem" disabled={disabled} onClick={handleStage}>
                     <Typography color="#fff">다음</Typography>
                 </Button>
             )}
@@ -80,5 +122,5 @@ const useStyles = makeStyles(theme => ({
         maxHeight: "844px",
         width: "100vw",
         padding: "24px 20px",
-    }
+    },
 }));
